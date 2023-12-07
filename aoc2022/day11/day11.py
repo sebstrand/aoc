5148da7262918ebf1962a339d9aeb7f24edc9b1f
Sebastian Strand <sebstrand@gmail.com>
2023-12-08 00:15:32 +0200

    2022/day11 wip

--- aoc2022/day11/day11.py
+++ aoc2022/day11/day11.py
@@ -1,5 +1,6 @@
 from aocl import *
 from collections import deque
+from functools import reduce
 
 
 def main():
@@ -9,44 +10,44 @@ def main():
     while len(lines) > 0:
         monkey = labeline(lines.popleft())
         monkeys[monkey.number] = {
-            'items': deque(ints(labeline(lines.popleft(), numbered=False).content, sep=(r', ',))),
+            'items': deque([factors(i) for i in ints(labeline(lines.popleft(), numbered=False).content, sep=(r', ',))]),
             'operation': splits(labeline(lines.popleft(), numbered=False).content[6:])[1:],
             'divisor': int(splits(labeline(lines.popleft(), numbered=False).content)[-1]),
             'target_true': int(splits(lines.popleft())[-1]),
             'target_false': int(splits(lines.popleft())[-1]),
             'inspections': 0,
-       }
+        }
+        # print(monkeys[monkey.number])
 
     p1 = False
     if p1:
         rounds = 20
-        decrease_divisor = 3
     else:
         # rounds = 10000
-        rounds = 50
-        decrease_divisor = 1
+        rounds = 1
 
-    for round in range(rounds):
-        print('round', round)
+    for rnd in range(rounds):
+        print('round', rnd)
         for turn in range(len(monkeys)):
-            # print('Turn: Monkey', turn)
+            print('Turn: Monkey', turn)
             monkey = monkeys[turn]
             items = monkey['items']
             while len(items):
                 monkey['inspections'] += 1
                 worry_level = items.popleft()
-                # print('  inspect', worry_level)
+                print('  inspect', worry_level)
                 worry_level = apply_op(monkey['operation'], worry_level)
-                # print('  increase to', worry_level)
-                worry_level = worry_level // decrease_divisor
-                # print('  decrease to', worry_level)
-                # print('  check', worry_level, 'divisible by', monkey['divisor'])
-                if worry_level % monkey['divisor'] == 0:
+                print('  increase to', worry_level)
+                if p1:
+                    worry_level = apply_op(('/', 3), worry_level)
+                    print('  decrease to', worry_level)
+                print('  check', worry_level, 'divisible by', monkey['divisor'])
+                if monkey['divisor'] in worry_level:
                     target_monkey = monkey['target_true']
-                    # print('  true, throw to', target_monkey)
+                    print('  true, throw to', target_monkey)
                 else:
                     target_monkey = monkey['target_false']
-                    # print('  false, throw to', target_monkey)
+                    print('  false, throw to', target_monkey)
                 monkeys[target_monkey]['items'].append(worry_level)
         print()
         for m in monkeys:
@@ -55,7 +56,7 @@ def main():
         print()
 
     inspections = [monkey['inspections'] for monkey in monkeys.values()]
-    print(inspections)
+    # print(inspections)
     inspections.sort(reverse=True)
     monkey_business = inspections[0] * inspections[1]
     print('monkey business:', monkey_business)
@@ -68,11 +69,17 @@ def main():
 def apply_op(operation, worry_level):
     op, num = operation
     if num == 'old':
-        num = worry_level
+        num = max(worry_level)
+    else:
+        num = int(num)
+
+    # print(op, num, worry_level)
     if op == '*':
-        return worry_level * int(num)
+        return factors(max(worry_level) * num)
     elif op == '+':
-        return worry_level + int(num)
+        return factors(max(worry_level) + num)
+    elif op == '/':
+        return factors(max(worry_level) // num)
 
 
 if __name__ == '__main__':
