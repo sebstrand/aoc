e64d5450074ec245cc5bf6a886c6bd5024965d16
Sebastian Strand <sebstrand@gmail.com>
2023-12-10 13:46:08 +0200

    2022/day11 done

--- aoc2022/day11/day11.py
+++ aoc2022/day11/day11.py
@@ -1,85 +1,81 @@
 from aocl import *
 from collections import deque
 from functools import reduce
+import operator
 
 
 def main():
-    lines = deque(read_lines('example'))
+    lines = deque(read_lines('input'))
 
     monkeys = {}
     while len(lines) > 0:
         monkey = labeline(lines.popleft())
         monkeys[monkey.number] = {
-            'items': deque([factors(i) for i in ints(labeline(lines.popleft(), numbered=False).content, sep=(r', ',))]),
+            'items': deque(ints(labeline(lines.popleft(), numbered=False).content, sep=(r', ',))),
             'operation': splits(labeline(lines.popleft(), numbered=False).content[6:])[1:],
             'divisor': int(splits(labeline(lines.popleft(), numbered=False).content)[-1]),
             'target_true': int(splits(lines.popleft())[-1]),
             'target_false': int(splits(lines.popleft())[-1]),
             'inspections': 0,
         }
-        # print(monkeys[monkey.number])
+
+    for i, m in enumerate(monkeys.values()):
+        print(f'm{i}', m)
+
+    divisor_product = reduce(operator.mul, (m['divisor'] for m in monkeys.values()))
+    print('divisor_product:', divisor_product)
 
     p1 = False
     if p1:
         rounds = 20
     else:
-        # rounds = 10000
-        rounds = 1
+        rounds = 10000
 
     for rnd in range(rounds):
-        print('round', rnd)
+        if rnd % 1000 == 0:
+            print('round', rnd)
         for turn in range(len(monkeys)):
-            print('Turn: Monkey', turn)
             monkey = monkeys[turn]
             items = monkey['items']
             while len(items):
                 monkey['inspections'] += 1
-                worry_level = items.popleft()
-                print('  inspect', worry_level)
-                worry_level = apply_op(monkey['operation'], worry_level)
-                print('  increase to', worry_level)
+                item = items.popleft()
+                item = apply_op(monkey['operation'], item) % divisor_product
                 if p1:
-                    worry_level = apply_op(('/', 3), worry_level)
-                    print('  decrease to', worry_level)
-                print('  check', worry_level, 'divisible by', monkey['divisor'])
-                if monkey['divisor'] in worry_level:
+                    item //= 3
+                if item % monkey['divisor'] == 0:
                     target_monkey = monkey['target_true']
-                    print('  true, throw to', target_monkey)
                 else:
                     target_monkey = monkey['target_false']
-                    print('  false, throw to', target_monkey)
-                monkeys[target_monkey]['items'].append(worry_level)
-        print()
-        for m in monkeys:
-            monkey = monkeys[m]
-            print(f'Monkey {m}:', [w for w in monkey['items']])
-        print()
+                monkeys[target_monkey]['items'].append(item)
 
     inspections = [monkey['inspections'] for monkey in monkeys.values()]
-    # print(inspections)
-    inspections.sort(reverse=True)
-    monkey_business = inspections[0] * inspections[1]
+    print('inspections:', inspections)
+    sorted_inspections = sorted(inspections, reverse=True)
+    monkey_business = sorted_inspections[0] * sorted_inspections[1]
     print('monkey business:', monkey_business)
     if p1:
         assert monkey_business == 78678
     else:
-        assert monkey_business == 0
+        assert inspections == [123834, 112260, 118063, 123821, 11603, 118089, 123818, 55724]
+        assert monkey_business == 15333249714
 
 
-def apply_op(operation, worry_level):
+def apply_op(operation, item):
     op, num = operation
     if num == 'old':
-        num = max(worry_level)
+        num = item
     else:
         num = int(num)
 
-    # print(op, num, worry_level)
     if op == '*':
-        return factors(max(worry_level) * num)
+        new_value = item * num
     elif op == '+':
-        return factors(max(worry_level) + num)
-    elif op == '/':
-        return factors(max(worry_level) // num)
+        new_value = item + num
+    else:
+        assert False
+
+    return new_value
 
 
 if __name__ == '__main__':
