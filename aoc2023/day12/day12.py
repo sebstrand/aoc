5080198cd0133b9286a37dd036255053c4e65a65
Sebastian Strand <sebstrand@gmail.com>
2023-12-15 16:51:18 +0200

    2023/day12 p2 maybe working (process pool)

--- aoc2023/day12/day12.py
+++ aoc2023/day12/day12.py
@@ -1,7 +1,7 @@
 from aocl import *
 import time
-from itertools import groupby
-from collections import Counter
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import math
 
 
@@ -9,147 +9,167 @@ expand = 5
 
 
 def main():
-    lines = [splits(s) for s in read_lines('input')]
+    records = [splits(s) for s in read_lines('input')]
+
+    if expand > 1:
+        for i, (springs, groups) in enumerate(records):
+            springs = springs.replace('.', '_').replace('?', '.')
+            groups = ints(groups)
+
+            springs = '.'.join([springs] * expand)
+            groups *= expand
+            records[i] = (springs, groups)
 
     start = time.time()
-    arrangement_sum = 0
-    for i, (springs, groups) in enumerate(lines):
-        groups = ints(groups)
-        if expand > 1:
-            springs = '?'.join([springs] * expand)
-            groups *= expand
-        groups = tuple(groups)
-        print('r', i, springs, groups)
+    total_arrangements = 0
 
-        gaps = len(groups) + 1
-        min_p_len = sum(groups) + gaps - 2
-        free_len = len(springs) - min_p_len
-        assert free_len >= 0
-        print('record', springs)
-        print('  rlen', len(springs), 'min_p_len', min_p_len, 'free', free_len, 'gaps', gaps)
+    # records = records[:201]
+    total_finished = 0
 
-        s_possible = []
-        for s in range(free_len):
-            test = springs[:s].replace('?', '.') + '#' * groups[0]
-            spring_pattern = re.compile(springs[:s + groups[0]].replace('.', r'\.').replace('?', '.'))
-            if test.index('#') < s:
-                break
-            elif spring_pattern.match(test):
-                print('  match', test)
-                s_possible.append(s)
-            pass
-        print('  s possible', s_possible)
+    with ProcessPoolExecutor() as executor:
+        future_to_idx = {
+            executor.submit(begin_arrange, springs, groups): i
+            for i, (springs, groups)
+            in enumerate(records)
+        }
+        for future in as_completed(future_to_idx):
+            total_finished += 1
+            idx = future_to_idx[future]
+            try:
+                num_arrangements = future.result()
+            except Exception as exc:
+                print('%r generated an exception: %s' % (idx, exc))
+            else:
+                total_arrangements += num_arrangements
+                springs, groups = records[idx]
+                print(f'r#{idx:04}:', springs)
+                print('  groups', groups)
+                print('  num arrangements:', num_arrangements, 'total now', total_arrangements)
+                print('  progress:', f'{math.floor(total_finished / len(records) * 100)}%',
+                      f'({len(records)-total_finished} remaining)')
 
-        gaparr = len(s_possible)
-        for sp in s_possible:
-            gaparr += space_gaps(free_len - sp, gaps - 1)
-        print('  gaparr', gaparr, '<', space_gaps(free_len, gaps))
-
-
-        e_possible = []
-        for s in range(free_len):
-            test = springs[:s].replace('?', '.') + '#' * groups[0]
-            spring_pattern = re.compile(springs[:s + groups[0]].replace('.', r'\.').replace('?', '.'))
-            if test.index('#') < s:
-                break
-            elif spring_pattern.match(test):
-                print('  match', test)
-                e_possible.append(s)
-            pass
-        print('  e possible', e_possible)
-
-        record_sum = 0
-        # for p in doit(springs, groups):
-        #     record_sum += 1
-        arrangement_sum += record_sum
-        print('  line arrangements:', record_sum)
-        if i >= 1000:
-            break
-
-    print('result:', arrangement_sum)
+    print()
+    print('total possible arrangements:', total_arrangements)
     print('time:', time.time() - start)
     if expand:
-        assert False
+        print('TODO: assert full result')
+        assert total_arrangements == 2113581  # first 200 with expand 3
     else:
-        assert arrangement_sum == 7007
+        assert total_arrangements == 7007
 
 
-def doit(record, groups, permutation=''):
-    anchor = record.find('#')
-    if len(groups) == 0:
-        if anchor < 0:
-            # One solution found
-            yield 1
-        else:
-            # There are unmatched groups
-            pass
-        return
-
-    n_open = record.count('?')
-    if n_open == 0 and anchor < 0:
-        # No way of generating missing groups
-        return
-
-    group_len = groups[0]
-    prefix = '.' * 0
-    suffix = '.' * 0
-
-    for x in range(420):
-        doit(record, groups[1:], permutation)
+def begin_arrange(springs, groups):
+    arrangement = np.zeros(len(groups)*2 + 1, dtype=np.byte) - 1
+    return arrange(arrangement, springs, groups)
 
 
-def space_gaps(spaces, gaps):
-    n = spaces + gaps - 1
-    k = gaps - 1
-    return int(math.factorial(n)/(math.factorial(k)*math.factorial(n-k)))
+def arrange(arrangement, springs, groups, indent=4):
+    # print(show_arrangement(arrangement), file=sys.stderr)
+    gs = groups[0]
+    ge = groups[-1]
 
+    unset = np.nonzero(arrangement < 0)[0]
+    if len(unset) < 1:
+        # print(' '*indent + 'no unset')
+        return 0
 
-def permutate_run(run, i=0):
-    if i == len(run):
-        yield ''
+    first_unset = unset[0]
+    last_unset = unset[-1]
+    # print(' '*indent + 'arrange', last_unset - first_unset + 1, show_arrangement(arrangement), 'of', groups)
+
+    gaps = len(groups) + 1
+    min_p_len = sum(groups) + gaps
+    if first_unset == 0:
+        # no outer gaps needed
+        min_p_len -= 2
+        min_outside_space = 0
     else:
-        chars = run[i]
-        if chars == '?':
-            chars = '#.'
+        min_outside_space = 1
 
-        for c in chars:
-            for p in permutate_run(run, i+1):
-                yield c + p
+    is_last = len(groups) <= 2
+    is_single = len(groups) == 1
 
+    used_before = arrangement[:first_unset].sum()
+    used_after = arrangement[last_unset + 1:].sum()
+    remaining_springs = springs[used_before:len(springs)-used_after]
+    free_len = len(remaining_springs) - min_p_len
+    # print(' '*indent + 'rs', remaining_springs, 'flen', free_len, 'minos', min_outside_space)
+    if free_len < 0:
+        # print(' '*indent + 'no 0, flen', free_len)
+        return 0
 
-def permutate(record, groups, permutation=None):
-    if not permutation:
-        permutation = []
-
-    c = record[len(permutation)]
-    if c == '?':
-        c = '#.'
-
-    permutation.append(None)
-    for x in c:
-        permutation[-1] = x
-        is_full = len(permutation) == len(record)
-        if check(permutation, groups, match=is_full):
-            if is_full:
-                yield 1
-            else:
-                for _ in permutate(record, groups, permutation):
-                    yield 1
-
-    permutation.pop()
-
-
-def check(permutation, groups, match=False):
-    groups_now = groups_in(permutation)
-    # print('  check', ''.join(permutation), groups_now, groups)
-    if match:
-        return groups_now == groups
+    num_arrangements = 0
+    if len(groups) > 2:
+        inner_groups = groups[1:-1]
     else:
-        return groups_now <= groups[:len(groups_now)]
+        inner_groups = None
+
+    for s in range(min_outside_space, min_outside_space + free_len + 1):
+        if 0 <= remaining_springs.find('#') < s:
+            break
+        test_s = '_' * s + '#' * gs
+
+        for e in range(min_outside_space, (min_outside_space + free_len + 1) - (s-min_outside_space)):
+            if remaining_springs.rfind('#', len(remaining_springs)-e) >= 0:
+                break
+            test_e = '#' * ge + '_' * e
+
+            if re.match(remaining_springs[:len(test_s)], test_s) and re.match(remaining_springs[-len(test_e):], test_e):
+                if is_last:
+                    if is_single and s + gs + e < len(remaining_springs):
+                        # print(' '*indent + '=> no 1')
+                        continue
+                    elif remaining_springs.find('#', len(test_s), len(remaining_springs)-len(test_e)) >= 0:
+                        # print(' '*indent + '=> no 2')
+                        continue
+
+                if is_single:  # (s, gs, e)
+                    # arrangement[first_unset] = s
+                    # arrangement[first_unset+1] = gs
+                    # arrangement[last_unset] = e
+                    # print(' '*indent + 'found single', (s, gs, e), num_arrangements + 1, show_arrangement(arrangement))
+                    # arrangement[first_unset:last_unset+1] = -1
+                    num_arrangements += 1
+                elif is_last:  # (s, gs, len(remaining), ge, e)
+                    # arrangement[first_unset] = s
+                    # arrangement[first_unset+1] = gs
+                    # arrangement[first_unset+2] = len(remaining)
+                    # arrangement[last_unset-1] = gs
+                    # arrangement[last_unset] = e
+                    # print(' '*indent + 'found last', (s, gs, len(remaining), gs, e), num_arrangements + 1, show_arrangement(arrangement))
+                    # arrangement[first_unset:last_unset+1] = -1
+                    num_arrangements += 1
+                elif inner_groups is None:  # (s, gs, ge, e)
+                    num_arrangements += 1
+                else:  # (s, gs, (subpattern), ge, e)
+                    arrangement[first_unset] = s
+                    arrangement[first_unset+1] = gs
+                    arrangement[last_unset-1] = ge
+                    arrangement[last_unset] = e
+                    # print(' '*indent + 'potential match, checking inner groups')
+                    num_arrangements += arrange(arrangement, springs, inner_groups, indent + 4)
+                    arrangement[first_unset:last_unset+1] = -1
+            # else:
+                # print(' '*indent + '=> no 3', test_s, remaining_springs[:len(test_s)], '*', test_e, remaining_springs[-len(test_e):], 'slen', len(remaining_springs))
+    # print(' '*indent + 'arrange complete', num_arrangements)
+    return num_arrangements
 
 
-def groups_in(p):
-    return tuple(len(tuple(g)) for k, g in groupby(p) if k != '.')
+def show_arrangement(arrangement, width=0):
+    def char(i, g):
+        if i % 2 == 0:
+            return '_' * g
+        return '#' * g
+
+    middle = len(arrangement) // 2
+    start = ''.join([char(i, g) for i, g in enumerate(arrangement[:middle])])
+    end = ''.join(reversed([char(i, g) for i, g in enumerate(reversed(arrangement[middle:]))]))
+    if min(arrangement) < 0 and width > 0:
+        return start + ' ' * (width - len(start) - len(end)) + end
+    elif len(arrangement) % 2 == 0 or min(arrangement) < 0:
+        return start + '|' + end
+    else:
+        return start + end
 
 
 if __name__ == '__main__':
