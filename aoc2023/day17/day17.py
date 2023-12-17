dd4dee99e24039f104d0f46495feba5b004cef14
Sebastian Strand <sebstrand@gmail.com>
2023-12-17 19:15:38 +0200

    2023/day17 wip

--- aoc2023/day17/day17.py
+++ aoc2023/day17/day17.py
@@ -1,6 +1,8 @@
-from aocl import *
-import numpy as np
 import sys
+import heapq as hq
+import itertools
+import numpy as np
+from aocl import *
 
 
 def solve(input_file, p1=True):
@@ -12,57 +14,220 @@ def solve(input_file, p1=True):
     for r, line in enumerate(lines):
         city[r] = [int(c) for c in line]
 
-    distance = np.zeros(city.shape, dtype=np.uint32) - 1
-    visited = np.zeros(city.shape, dtype=np.byte)
+    end_pos = (rows-1, cols-1)
+    distance, reached_with_move = a_star2(city, (0, 0), end_pos)
 
-    pos = (0, 0)
-    target_pos = (rows - 1, cols - 1)
-    distance[pos] = 0
-
-    previous = {}
-    while pos:
-        visited[pos] = True
-        if pos == target_pos:
-            break
-
-        path_to_pos = [pos]
-        prev_pos = previous.get(pos)
-        if prev_pos:
-            path_to_pos.append(prev_pos)
-
-        unvisited_neighbors = [n for n in neighbors_2d(city, pos, valid_only=True) if not visited[n[0]]]
-
-        for n_pos, heat_loss in unvisited_neighbors:
-            if len(path_to_pos) == 2:
-                rows_3 = [n_pos[0]] + [r for r, _ in path_to_pos]
-                cols_3 = [n_pos[1]] + [c for _, c in path_to_pos]
-                if min(rows_3) == max(rows_3) or min(cols_3) == max(cols_3):
-                    print('blocking 4 straight')
-                    continue
-            n_dist = distance[pos] + heat_loss
-            distance[n_pos] = min(distance[n_pos], n_dist)
-            previous[n_pos] = pos
-
-        next_pos = None
-        unvisited = list(zip(*np.where(visited == 0)))
-        for unvisited_pos in unvisited:
-            if not next_pos or distance[unvisited_pos] < distance[next_pos]:
-                next_pos = unvisited_pos
-
-        pos = next_pos
-
-    print('City:\n', city)
-    print('Visited:\n', visited)
-    print('Distances+1:\n', distance + 1)
+    print('City:')
+    draw_path2(city, {}, end_pos)
+    print('Path:')
+    draw_path2(city, reached_with_move, end_pos)
+    # print('Path:\n', path)
+    # print('Visited:\n', visited)
+    # print('Distance:\n', distance.clip(0, 9999))
 
     if p1:
-        return distance[-1, -1]
+        return distance[end_pos]
+
+
+def a_star2(city, start_pos, end_pos):
+    distance = np.zeros(city.shape, dtype=np.uint32) - 1
+    distance[start_pos] = 0
+
+    reached_with_move = {}
+    frontier = PriorityQueue()
+    frontier.add(estimate_distance(city, start_pos, end_pos), start_pos)
+
+    while len(frontier) > 0:
+        current_pos = frontier.pop()
+        if current_pos == end_pos:
+            break
+
+        for move, move_cost, target_pos in possible_moves(city, reached_with_move, current_pos):
+            tentative_distance = distance[current_pos] + move_cost
+            if tentative_distance < distance[target_pos]:
+                # print('yes', move, target_pos, move_cost)
+                reached_with_move[target_pos] = move, current_pos
+                distance[target_pos] = tentative_distance
+                frontier.add(tentative_distance + estimate_distance(city, target_pos, end_pos), target_pos)
+
+    return distance, reached_with_move
+
+
+def possible_moves(city, reached_with_move, current_pos):
+    rows, cols = city.shape
+
+    previous_moves = ''
+    pos = current_pos
+    while True:
+        previous_move, pos = reached_with_move.get(pos, (None, None))
+        if not pos:
+            break
+        previous_moves = previous_move + previous_moves
+        if len(previous_moves) >= 4:
+            break
+
+    moves = (
+        list(itertools.product('NSWE', repeat=3)) +
+        list(itertools.product('NSWE', repeat=2)) +
+        list('NSWE')
+    )
+    for move in moves:
+        move = ''.join(move)
+        combined = (previous_moves + move)
+        if 'NNNN' in combined or 'SSSS' in combined or 'WWWW' in combined or 'EEEE' in combined:
+            # invalid move (4 straight)
+            continue
+        elif 'NS' in combined or 'SN' in combined or 'EW' in combined or 'WE' in combined:
+            # invalid move (immediate reverse)
+            continue
+        else:
+            move_cost = 0
+            r, c = current_pos
+            for m in move:
+                if m == 'N':
+                    r -= 1
+                elif m == 'S':
+                    r += 1
+                elif m == 'W':
+                    c -= 1
+                elif m == 'E':
+                    c += 1
+                if r < 0 or r >= rows or c < 0 or c >= cols:
+                    move = None
+                    break
+                move_cost += city[r, c]
+            if move:
+                yield move, move_cost, (r, c)
+
+
+def a_star(city, start_pos, end_pos):
+    distance = np.zeros(city.shape, dtype=np.uint32) - 1
+    distance[start_pos] = 0
+
+    came_from = {}
+    frontier = PriorityQueue()
+    frontier.add(estimate_distance(city, start_pos, end_pos), start_pos)
+
+    while len(frontier) > 0:
+        current_pos = frontier.pop()
+        if current_pos == end_pos:
+            break
+        for n_pos, heat_loss in neighbors_2d(city, current_pos, valid_only=True):
+            p1 = came_from.get(current_pos)
+            p2 = came_from.get(p1)
+            p3 = came_from.get(p2)
+            # if p1 and p2 and p3:
+            #     if n_pos[0] == current_pos[0] == p1[0] == p2[0] == p3[0]:
+            #         # n on same row
+            #         continue
+            #     elif n_pos[1] == current_pos[1] == p1[1] == p2[1] == p3[1]:
+            #         # n in same col
+            #         continue
+            if n_pos == p1:
+                print('=> block reverse ?', current_pos, n_pos)
+                continue
+
+            tentative_distance = distance[current_pos] + heat_loss
+            if tentative_distance < distance[n_pos]:
+                came_from[n_pos] = current_pos
+                distance[n_pos] = tentative_distance
+                frontier.add(tentative_distance + estimate_distance(city, n_pos, end_pos), n_pos)
+
+    return distance, came_from
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.entries = {}
+        self.pq = []
+
+    def __len__(self):
+        return len(self.entries)
+
+    def add(self, priority, item):
+        if item in self.entries:
+            entry = self.entries.pop(item)
+            entry[-1] = ()
+        entry = [priority, item]
+        self.entries[item] = entry
+        hq.heappush(self.pq, entry)
+
+    def pop(self):
+        while self.pq:
+            priority, item = hq.heappop(self.pq)
+            if item:
+                del self.entries[item]
+                return item
+        raise KeyError('priority queue is empty')
+
+
+def estimate_distance(city, pos1, pos2):
+    if pos1 == pos2:
+        return 0
+    # n = neighbors_2d(city, pos1)
+    # south = n[1]
+    # east = n[3]
+    # if south and east:
+    #     m = min((south[1], east[1]))
+    # elif not east:
+    #     m = south[1]
+    # else:
+    #     m = east[1]
+    # return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) + m - 1
+    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+
+def draw_path(city, came_from, pos):
+    path = set()
+    while pos:
+        path.add(pos)
+        pos = came_from.get(pos)
+    rows, cols = city.shape
+    for r in range(rows):
+        for c in range(cols):
+            on_path = (r, c) in path and (r, c) != (0, 0)
+            print((city[r, c], '.')[on_path], end='')
+        print()
+
+
+def draw_path2(city, reached_with_move, pos):
+    path = {}
+    while True:
+        move, _ = reached_with_move.get(pos, (None, None))
+        if not move:
+            break
+
+        r, c = pos
+        for m in reversed(move):
+            if m == 'N':
+                path[(r, c)] = '^'
+                r += 1
+            elif m == 'S':
+                path[(r, c)] = 'v'
+                r -= 1
+            elif m == 'W':
+                path[(r, c)] = '<'
+                c += 1
+            elif m == 'E':
+                path[(r, c)] = '>'
+                c -= 1
+        pos = (r, c)
+
+    rows, cols = city.shape
+    for r in range(rows):
+        for c in range(cols):
+            move = path.get((r, c))
+            if move:
+                print(path[(r, c)], end='')
+            else:
+                print(city[r, c], end='')
+        print()
 
 
 def main():
     _input_file = 'example'
     expected = {
-        'input': (None, None),
+        'input': (None, None),  # less than 692 for p1
         'example': (102, None),
     }[_input_file]
 
