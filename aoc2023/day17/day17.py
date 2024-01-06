5a1e1516257be28bdf117428389716c43885c7c2
Sebastian Strand <sebstrand@gmail.com>
2024-01-06 18:55:43 +0200

    2023/day17-p2 work in progress

--- aoc2023/day17/day17.py
+++ aoc2023/day17/day17.py
@@ -10,41 +10,35 @@ def solve(input_file, p1=True):
     lines = read_lines(input_file)
     rows, cols = len(lines), len(lines[0])
 
-    np.set_printoptions(threshold=sys.maxsize)
+    np.set_printoptions(threshold=sys.maxsize, linewidth=1000)
     city = np.zeros((rows, cols), dtype=np.byte)
     for r, line in enumerate(lines):
         city[r] = [int(c) for c in line]
 
+    start_pos = (0, 0)
     end_pos = (rows-1, cols-1)
-    # distance, reached_with_move = a_star(city, (0, 0), end_pos)
-    heat_loss, path = dijkstra(city, (0, 0), end_pos)
-    # for move, distance in distances.items():
-    #     print('move', move, '\n', distance.clip(0, 999))
-
-    # print('City:')
-    # draw_path(city, {}, end_pos)
-    # print('Path:')
-    # draw_path(city, path)
-    # draw_path(city, reached_with_move, end_pos)
-    # print('moves', moves)
-    # print('Path:\n', path)
-    # print('Visited:\n', visited)
-    # print('Distance:\n', distance.clip(0, 9999))
-
     if p1:
-        return heat_loss
+        is_valid_move = is_valid_move_p1
+        length_limit = 1
+    else:
+        is_valid_move = is_valid_move_p2
+        length_limit = 4
 
+    distances = dijkstra(city, start_pos, is_valid_move, length_limit)
+    for move, distance in distances.items():
+        if len(move) == length_limit:
+            print(move, '\n', distance.clip(0, 999))
+    distances_list = list(distances.values())
+    d = distances_list[0]
+    for x in distances_list[1:]:
+        d = np.minimum(d, x)
+    print('min dist:\n', d.clip(0, 999))
 
-def find_first_illegal_move(moves):
-    for i in range(len(moves)):
-        last4 = moves[i-4:i]
-        if 'NNNN' in last4 or 'SSSS' in last4 or 'WWWW' in last4 or 'EEEE' in last4:
-            # invalid move (4 straight)
-            return i
-        elif 'NS' in last4 or 'SN' in last4 or 'EW' in last4 or 'WE' in last4:
-            # invalid move (immediate reverse)
-            return i
-    return -1
+    heat_loss, path = construct_optimal_path_dfs(city, start_pos, end_pos, distances, is_valid_move)
+
+    print('Path:')
+    draw_path(city, path)
+    return heat_loss
 
 
 def path_to_moves(path):
@@ -63,60 +57,152 @@ def path_to_moves(path):
     return ''.join(moves)
 
 
-def dijkstra(city, start_pos, end_pos):
-    moves = (
-            [''.join(m) for m in itertools.product('NSWE', repeat=3)]
-            + [''.join(m) for m in itertools.product('NSWE', repeat=2)]
-            + ['N', 'S', 'W', 'E']
-    )
-    valid_moves = [
-        m for m
-        in moves
-        if 'NS' not in m and 'SN' not in m and 'WE' not in m and 'EW' not in m
-    ]
+def construct_optimal_path_dfs(city, start_pos, end_pos, distances, is_valid_move, min_solutions=999):
+    rows, cols = city.shape
 
-    distances = {}
-    for move in valid_moves:
-        distances[move] = np.zeros(city.shape, dtype=np.uint32) - 1
-    max_dist = distances['NNN'][0, 0]
+    q = deque()
+    initial_path = deque()
+    initial_path.append(end_pos)
+    q.append((0 + city[end_pos], initial_path))
 
-    q = PriorityQueue()
-    q.add(0, (start_pos, ''))
+    max_path_len = 2 * (len(city) + len(city[0]))
+    max_dist = 2**32 - 1
+    best_heat_loss = 2**32 - 1
+
+    found_paths = []
+    i = 0
     while len(q) > 0:
-        pos, pos_move = q.pop()
-        for direction in 'NSWE':
-            move4 = (pos_move + direction)[-4:]
-            if not is_valid_move(move4):
-                continue
+        heat_loss, path = q.pop()
+        path_moves = path_to_moves(path)
+        # print('pos', path[0])
 
-            n_pos = go_in_direction(pos, direction)
-            if not (0 <= n_pos[0] < len(city)) or not (0 <= n_pos[1] < len(city[0])):
-                continue
+        if i % 1000 == 0 and False:
+            print('qlen', len(q), 'plen', len(path), 'with heat loss', heat_loss, 'pos', path[0])
+        i += 1
+        # if i > 200:
+        #     break
 
-            heat_loss = city[n_pos]
-            move3 = move4[-3:]
-            if pos_move in distances:
-                distance_to_n = distances[pos_move][pos] + heat_loss
-            else:
-                distance_to_n = heat_loss
+        next_hops = []
+        for move, distance in distances.items():
+            full_move = move + path_moves
 
-            if distance_to_n < distances[move3][n_pos]:
-                distances[move3][n_pos] = distance_to_n
-                q.add(distance_to_n, (n_pos, move3))
+            if is_valid_move(full_move, incomplete=False):
+                pos = path[0]
+                new_path = deque(path)
+                new_heat_loss = heat_loss
+                for direction in reversed(move):
+                    pos = go_in_direction(pos, direction, reverse=True)
+                    if not (0 <= pos[0] < rows and 0 <= pos[1] < cols):
+                        pos = None
+                        break
+                    elif pos in new_path:
+                        pos = None
+                        break
 
+                    new_path.appendleft(pos)
+                    new_heat_loss += city[pos]
+
+                if pos:
+                    if new_heat_loss > best_heat_loss:
+                        pass
+                    elif pos == start_pos:
+                        best_heat_loss = new_heat_loss
+                        found_paths.append((new_heat_loss - city[start_pos], new_path))
+                        print('found path', found_paths[-1])
+                        # return found_paths[0]
+                    elif len(new_path) >= max_path_len:
+                        pass
+                    elif city[pos] < max_dist and distance[path[0]] < max_dist:
+                        # print('depth', len(new_path), new_heat_loss, abs(pos[0] - start_pos[0]) + abs(pos[1] - start_pos[1]))
+                        # next_hops.append((distance[path[0]], new_heat_loss, new_path))
+                        next_hops.append((distance[path[0]], len(next_hops), new_heat_loss, new_path))
+
+        for hop in sorted(next_hops, reverse=True):
+            # q.append(hop[1:])
+            q.append(hop[2:])
+
+        if len(found_paths) > min_solutions:
+            break
+
+    found_paths.sort()
+    for path in found_paths:
+        print('found path', path)
+    return found_paths[0]
+
+
+def construct_optimal_path_bfs(city, start_pos, end_pos, distances, is_valid_move):
+    q = deque()
+    initial_path = deque()
+    initial_path.append(end_pos)
+    q.append((end_pos, 0 + city[end_pos], initial_path))
+
+    max_path_len = len(city) + len(city[0])
+    max_dist = 2**32 - 1
+    found_paths = []
+    i = 0
+    while len(q) > 0:
+        if i > 0 and i % 5000 == 0:
+            print('qlen', len(q))
+        i += 1
+        pos, heat_loss, path = q.popleft()
+        # print('pp', pos, path)
+        path_moves = path_to_moves(path)
+        for move, distance in distances.items():
+            full_move = move + path_moves
+            if is_valid_move(full_move, incomplete=False):
+                # print('  move', move)
+                move_pos = pos
+                new_path = deque(path)
+                new_heat_loss = heat_loss
+                for direction in reversed(move):
+                    move_pos = go_in_direction(move_pos, direction, reverse=True)
+                    if not (0 <= move_pos[0] < len(city) and 0 <= move_pos[1] < len(city[0])):
+                        move_pos = None
+                        break
+                    elif move_pos in new_path:
+                        move_pos = None
+                        break
+
+                    new_path.appendleft(move_pos)
+                    new_heat_loss += city[move_pos]
+
+                if move_pos:
+                    if i > 0 and i % 5000 == 0:
+                        print('nhl', new_heat_loss, '@pl', len(new_path))
+
+                    if move_pos == start_pos:
+                        found_paths.append((new_heat_loss - city[start_pos], new_path))
+                    elif len(path) > max_path_len:
+                        pass
+                    elif new_heat_loss > 1000:
+                        print('crucible too cold')
+                        pass
+                    elif city[move_pos] < max_dist:
+                        q.append((move_pos, new_heat_loss, new_path))
+
+    found_paths.sort()
+    # for path in found_paths:
+    #     print('found path', path)
+    return found_paths[0]
+
+
+def construct_optimal_path_old(city, start_pos, end_pos, distances, is_valid_move):
     pos = end_pos
     path = deque()
     path.append(pos)
     best_move = ''
+    max_dist = 2**32 - 1
     while pos and pos != start_pos:
-        move_after = best_move
+        print('pos', pos)
         min_dist = max_dist
+        path_moves = path_to_moves(path)
         for move, distance in distances.items():
-            if is_valid_move(move + move_after) and distance[pos] < min_dist:
+            combined = move + path_moves
+            if is_valid_move(combined, incomplete=False) and distance[pos] < min_dist:
                 min_dist = distance[pos]
                 best_move = move
         if min_dist == max_dist:
-            raise 'path broken'
+            raise Exception('incomplete path')
 
         for direction in reversed(best_move):
             pos = go_in_direction(pos, direction, reverse=True)
@@ -126,7 +212,38 @@ def dijkstra(city, start_pos, end_pos):
     return sum(city[pos] for pos in path[1:]), path
 
 
-def is_valid_move(move):
+def dijkstra(city, start_pos, is_valid_move, length_limit):
+    distances = defaultdict(lambda: np.zeros(city.shape, dtype=np.uint32) - 1)
+    distances['S'*length_limit][start_pos] = 0
+
+    q = PriorityQueue()
+    q.add(0, (start_pos, ''))
+    while len(q) > 0:
+        pos, pos_move = q.pop()
+        for direction in 'NSWE':
+            planned_move = pos_move + direction
+            if not is_valid_move(planned_move):
+                continue
+
+            n_pos = go_in_direction(pos, direction)
+            if n_pos == start_pos or not (0 <= n_pos[0] < len(city)) or not (0 <= n_pos[1] < len(city[0])):
+                continue
+
+            heat_loss = city[n_pos]
+            limited_move = planned_move[-length_limit:]
+            if pos_move in distances:
+                distance_to_n = distances[pos_move][pos] + heat_loss
+            else:
+                distance_to_n = heat_loss
+
+            if distance_to_n < distances[limited_move][n_pos]:
+                distances[limited_move][n_pos] = distance_to_n
+                q.add(distance_to_n, (n_pos, limited_move))
+
+    return distances
+
+
+def is_valid_move_p1(move, incomplete=True):
     if 'NNNN' in move or 'SSSS' in move or 'WWWW' in move or 'EEEE' in move:
         # invalid move: 4 straight
         return False
@@ -134,6 +251,30 @@ def is_valid_move(move):
         # invalid move: immediate reverse
         return False
     return True
+
+
+def is_valid_move_p2(move, incomplete=True):
+    if len(move) == 1:
+        return True
+    elif 'NS' in move or 'SN' in move or 'EW' in move or 'WE' in move:
+        return False
+    elif len(move) >= 10 and ('N'*10 in move or 'S'*10 in move or 'W'*10 in move or 'E'*10 in move):
+        return False
+
+    if incomplete:
+        if move[-1] == move[-2]:
+            return True
+        else:
+            if len(move) < 5:
+                return False
+            preceding = move[-6:-2]
+        return preceding == move[-2] * len(preceding)
+    else:
+        if len(move) < 5:
+            return move == move[0] * len(move)
+        else:
+            groups = [len(list(g)) for _, g in itertools.groupby(move)]
+            return min(groups) >= 4
 
 
 def go_in_direction(pos, direction, reverse=False):
@@ -151,215 +292,6 @@ def go_in_direction(pos, direction, reverse=False):
     return r, c
 
 
-def possible_moves_d(city, previous, current_pos, allow_illegal=False):
-    rows, cols = city.shape
-
-    previous_moves = deque()
-    pos = current_pos
-    pos_after = None
-    while pos and len(previous_moves) < 4:
-        if pos_after:
-            r1, c1 = pos
-            r2, c2 = pos_after
-            if r1 < r2:
-                previous_moves.append('S')
-            elif r1 > r2:
-                previous_moves.append('N')
-            elif c1 < c2:
-                previous_moves.append('E')
-            elif c1 > c2:
-                previous_moves.append('W')
-
-        pos_after = pos
-        pos = previous.get(pos)
-
-    previous_moves = ''.join(previous_moves)
-    print('  previous moves', previous_moves)
-
-    moves = (
-        list(itertools.product('NSWE', repeat=3))
-        # list(itertools.product('NSWE', repeat=2)) +
-        # list('NSWE')
-    )
-    for move in moves:
-        move = ''.join(move)
-        combined = (previous_moves + move)
-        if not allow_illegal and ('NNNN' in combined or 'SSSS' in combined or 'WWWW' in combined or 'EEEE' in combined):
-            # invalid move (4 straight)
-            continue
-        elif not allow_illegal and ('NS' in combined or 'SN' in combined or 'EW' in combined or 'WE' in combined):
-            # invalid move (immediate reverse)
-            continue
-        else:
-            move_cost = 0
-            r, c = current_pos
-            for m in move:
-                if m == 'N':
-                    r -= 1
-                elif m == 'S':
-                    r += 1
-                elif m == 'W':
-                    c -= 1
-                elif m == 'E':
-                    c += 1
-                if r < 0 or r >= rows or c < 0 or c >= cols:
-                    move = None
-                    break
-                move_cost += city[r, c]
-            if move:
-                yield (r, c), move_cost
-
-
-def a_star(city, start_pos, end_pos):
-    distance = np.zeros(city.shape, dtype=np.uint32) - 1
-    distance[start_pos] = 0
-
-    reached_with_move = defaultdict(list)
-    frontier = PriorityQueue()
-    frontier.add(estimate_distance(city, start_pos, end_pos), start_pos)
-
-    while len(frontier) > 0:
-        current_pos = frontier.pop()
-        if current_pos == end_pos:
-            break
-
-        for move, move_cost, target_pos in possible_moves(city, reached_with_move, current_pos):
-            tentative_distance = distance[current_pos] + move_cost
-            if tentative_distance < distance[target_pos]:
-                # print('yes', move, target_pos, move_cost)
-                reached_with_move[target_pos].append((move, current_pos))
-                distance[target_pos] = tentative_distance
-                frontier.add(tentative_distance + estimate_distance(city, target_pos, end_pos), target_pos)
-            elif tentative_distance == distance[target_pos]:
-                reached_with_move[target_pos].append((move, current_pos))
-
-    return distance, reached_with_move
-
-
-def possible_moves(city, reached_with_move, current_pos, allow_illegal=False):
-    rows, cols = city.shape
-
-    previous_moves = ''
-    pos = current_pos
-    while True:
-        previous_move, pos = reached_with_move.get(pos, (None, None))
-        if not pos:
-            break
-        previous_moves = previous_move + previous_moves
-        if len(previous_moves) >= 4:
-            break
-
-    moves = (
-        # list(itertools.product('NSWE', repeat=3)) +
-        # list(itertools.product('NSWE', repeat=2)) +
-        list('NSWE')
-    )
-    for move in moves:
-        move = ''.join(move)
-        combined = (previous_moves + move)
-        if not allow_illegal and ('NNNN' in combined or 'SSSS' in combined or 'WWWW' in combined or 'EEEE' in combined):
-            # invalid move (4 straight)
-            continue
-        elif not allow_illegal and ('NS' in combined or 'SN' in combined or 'EW' in combined or 'WE' in combined):
-            # invalid move (immediate reverse)
-            continue
-        else:
-            move_cost = 0
-            r, c = current_pos
-            for m in move:
-                if m == 'N':
-                    r -= 1
-                elif m == 'S':
-                    r += 1
-                elif m == 'W':
-                    c -= 1
-                elif m == 'E':
-                    c += 1
-                if r < 0 or r >= rows or c < 0 or c >= cols:
-                    move = None
-                    break
-                move_cost += city[r, c]
-            if move:
-                yield move, move_cost, (r, c)
-
-
-def estimate_distance(city, pos1, pos2):
-    if pos1 == pos2:
-        return 0
-    # n = neighbors_2d(city, pos1)
-    # south = n[1]
-    # east = n[3]
-    # if south and east:
-    #     m = min((south[1], east[1]))
-    # elif not east:
-    #     m = south[1]
-    # else:
-    #     m = east[1]
-    # return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]) + m - 1
-    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
-
-
-def draw_path_astar(city, reached_with_move, pos):
-    path = {}
-    prev = None
-    lc = ' '
-    while True:
-        move, _ = reached_with_move.get(pos, (None, None))
-        if not move:
-            break
-
-        r, c = pos
-        for m in reversed(move):
-            cur_c, cur_r = c, r
-            if m == 'N':
-                if prev == 'E':
-                    lc = '┌'
-                elif prev == 'W':
-                    lc = '┐'
-                else:
-                    lc = '│'
-                r += 1
-            elif m == 'S':
-                if prev == 'E':
-                    lc = '└'
-                elif prev == 'W':
-                    lc = '┘'
-                else:
-                    lc = '│'
-                r -= 1
-            elif m == 'W':
-                if prev == 'N':
-                    lc = '└'
-                elif prev == 'S':
-                    lc = '┌'
-                else:
-                    lc = '─'
-                c += 1
-            elif m == 'E':
-                if prev == 'N':
-                    lc = '┘'
-                elif prev == 'S':
-                    lc = '┐'
-                else:
-                    lc = '─'
-                c -= 1
-            if prev is not None:
-                path[(cur_r, cur_c)] = lc
-
-            prev = m
-        pos = (r, c)
-
-    rows, cols = city.shape
-    for r in range(rows):
-        for c in range(cols):
-            move = path.get((r, c))
-            if move:
-                print(path[(r, c)], end='')
-            else:
-                print(city[r, c], end='')
-        print()
-
-
 def draw_path(city, path):
     path_set = set(path)
     rows, cols = city.shape
@@ -373,10 +305,11 @@ def draw_path(city, path):
 
 
 def main():
-    _input_file = 'input'
+    _input_file = 'example'
     expected = {
-        'input': (686, None),  # less than 692 for p1, A* gives 569 with no move restrictions (too low)
-        'example': (102, None),
+        'input': (686, None),  # 780 < p2 < 832,
+        'example': (102, 94),
+        'example2': (59, 71),
     }[_input_file]
 
     run(__file__, solve, _input_file, expected[0], p1=True)
