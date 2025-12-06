1e8dcf149dc865650dec5ecf36151ec29ceb6f28
Sebastian Strand <sebstrand@gmail.com>
2025-12-06 10:03:43 +0200

    2025:day03: optimize

--- 2025/03/day3.py
+++ 2025/03/day3.py
@@ -1,4 +1,3 @@
-import functools
 from aocl import *
 
 
@@ -6,22 +5,14 @@ def solve(input_file, p1=True):
     banks = read_lines(input_file)
     max_total_joltage = 0
     for bank in banks:
-        max_total_joltage += find_max_joltage(bank, 2 if p1 else 12)
+        max_total_joltage += int(find_max_joltage(bank, 2 if p1 else 12))
     return max_total_joltage
 
 
-@functools.cache
 def find_max_joltage(bank, n):
-    max_joltage = 0
-    for i, joltage_a in enumerate(bank[:len(bank)-n+1]):
-        if n > 1:
-            joltage_b = str(find_max_joltage(bank[i + 1:], n-1))
-            joltage = int(joltage_a + joltage_b)
-        else:
-            joltage = int(joltage_a)
-        if joltage > max_joltage:
-            max_joltage = joltage
-    return max_joltage
+    joltage_a = str(max(int(b) for b in bank[:len(bank)-n+1]))
+    a_pos = bank.index(joltage_a)
+    return joltage_a + (find_max_joltage(bank[a_pos + 1:], n-1) if n > 1 else '')
 
 
 def main():
